e574930a9e8149748330671be13d29d7928db9fb
Luis Sierra <luis.ed.sierra@gmail.com>
2022-06-02 16:12:56 -0500

    feat: moved code into a consumer

--- src/main/java/com/eqfx/latam/poc/Main.java
+++ src/main/java/com/eqfx/latam/poc/Main.java
@@ -1,17 +1,9 @@
 package com.eqfx.latam.poc;
 
-import com.eqfx.latam.poc.csv.CSVRecordMap;
-import com.eqfx.latam.poc.csv.CsvIO;
-import com.eqfx.latam.poc.csv.CsvParsers;
-import com.eqfx.latam.poc.model.SaleOrder;
 import com.eqfx.latam.poc.scenario.SalesByQuarter;
+import com.eqfx.latam.poc.scenario.ScenarioTwoConsumer;
 import org.apache.beam.sdk.Pipeline;
-import org.apache.beam.sdk.io.AvroIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
-import org.apache.beam.sdk.values.PCollection;
-
-import static com.eqfx.latam.poc.csv.util.CsvConstants.DELIMITER_SCENARIO_2;
-import static com.eqfx.latam.poc.csv.util.CsvConstants.HEADERS_SCENARIO_2;
 
 public class Main {
     public static void main(String[] args) {
@@ -31,20 +23,9 @@ public class Main {
                 //TODO save to AVRO
                 break;
             }
-            case TWO: {
-                PCollection<CSVRecordMap> csvRecordMap = pipeline.apply("Reading from csv",
-                        CsvIO.read(options.getSourceFile())
-                                .withDelimiter(DELIMITER_SCENARIO_2)
-                                .withHeaders(HEADERS_SCENARIO_2)
-                                .build()
-                );
-                PCollection<SaleOrder> csvMapped = csvRecordMap.apply("Parse to Sale", CsvParsers.saleOrders());
-                PCollection<SalesByQuarter.Result> result = SalesByQuarter.apply(options.as(SalesByQuarter.Options.class), csvMapped);
-                result.apply("Save to avro", AvroIO.write(SalesByQuarter.Result.class)
-                        .to(options.getTargetFile())
-                        .withSuffix(".avro"));
+            case TWO:
+                new ScenarioTwoConsumer(options.as(SalesByQuarter.Options.class)).accept(pipeline);
                 break;
-            }
         }
         pipeline.run();
     }
