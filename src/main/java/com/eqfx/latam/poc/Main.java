baebad7ba2911a71f020e38220c0f1564888a134
Jorge_Lopez <joanlopezto@gmail.com>
2022-06-02 15:59:53 -0500

    Scenario 1 code.

--- src/main/java/com/eqfx/latam/poc/Main.java
+++ src/main/java/com/eqfx/latam/poc/Main.java
@@ -1,7 +1,13 @@
 package com.eqfx.latam.poc;
 
+import com.eqfx.latam.poc.csv.CSVRecordMap;
+import com.eqfx.latam.poc.csv.CsvIO;
+import com.eqfx.latam.poc.csv.CsvParsers;
+import com.eqfx.latam.poc.model.Product;
+import com.eqfx.latam.poc.model.SaleOrder;
 import com.eqfx.latam.poc.scenario.ProductAvgPrice;
 import com.eqfx.latam.poc.scenario.SalesByQuarter;
+import com.eqfx.latam.poc.util.Log;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.AvroIO;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
@@ -21,10 +27,29 @@ public class Main {
         switch (options.getScenario()){
             case ONE: {
                 //TODO parse the csv model to the product model
+                PCollection<CSVRecordMap> csvRecordMap = pipeline.apply("Reading from CSV",
+                        CsvIO.read(options.getSourceFile())
+                                .withDelimiter(';')
+                                .withHeaders(	"ProductID", "ProductName",	"ProductNumber",	"MakeFlag",	"FinishedGoodsFlag",	"Color",
+                                        "SafetyStockLevel",	"ReorderPoint",	"StandardCost",	"ListPrice",	"Size",
+                                        "SizeUnitMeasureCode",	"WeightUnitMeasureCode",	"Weight",	"DaysToManufacture",
+                                        "ProductLine",	"Class",	"Style",	"ProductSubcategoryID",	"ProductModelID",
+                                        "SellStartDate",	"SellEndDate",	"DiscontinuedDate",	"SalesOrderDetailID",
+                                        "CarrierTrackingNumber",	"OrderQty",	"SpecialOfferID",	"UnitPrice",	"UnitPriceDiscount",
+                                        "LineTotal"
+                                )
+                                .build()
+                );
+
+                PCollection<Product> csvMapped = csvRecordMap.apply("Parse to Product",
+                        CsvParsers.products());
 
                 //TODO call the correct scenario e.j. ProductAvgPrice.apply(...)
                 PCollection<ProductAvgPrice.Result> result = ProductAvgPrice
-                        .apply( (ProductAvgPrice.Options) options, placeholder);
+                        .apply(options.as(ProductAvgPrice.Options.class), csvMapped);
+
+                result.apply(Log.ofElements());
+
                 //TODO save to AVRO
                 result.apply("Save to AVRO",
                         AvroIO.write(ProductAvgPrice.Result.class)
--- src/main/java/com/eqfx/latam/poc/csv/CsvParsers.java
+++ src/main/java/com/eqfx/latam/poc/csv/CsvParsers.java
@@ -1,9 +1,13 @@
 package com.eqfx.latam.poc.csv;
 
+import com.eqfx.latam.poc.model.Product;
 import com.eqfx.latam.poc.model.SaleOrder;
+import com.eqfx.latam.poc.scenario.ProductAvgPrice;
+import org.apache.beam.repackaged.core.org.antlr.v4.runtime.misc.IntegerList;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 
+import java.math.RoundingMode;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -18,6 +22,16 @@ public class CsvParsers {
             Money unitPrice = Money.of(CurrencyUnit.USD, Double.parseDouble(input.get("UnitPrice")));
             Integer qty = Integer.valueOf(input.get("OrderQty"));
             return new SaleOrder(category,subCategory, date, unitPrice, qty);
+        });
+    }
+
+    public static CsvParser<Product> products(){
+        return CsvParser.of(Product.class).using(input -> {
+            Integer id = Integer.parseInt(input.get("ProductID"));
+            String name = input.get("ProductName");
+            Money unitPrice = Money.of(CurrencyUnit.USD, Double.parseDouble(
+                    input.get("UnitPrice").replace(',','.')), RoundingMode.HALF_UP);
+            return new Product(id, name, unitPrice);
         });
     }
 }
